6dcabe27fc12207d68ee765333c602749cab88b7
misaelmtz10 <85245368+misaelmtz10@users.noreply.github.com>
2022-04-01 17:05:00 -0600

    Controlador home y admin
    Se agregaron los controladores vacíos con el método index.

---
+++ src/main/java/utez/edu/mx/citas/controller/AdminController.java
@@ -0,0 +1,21 @@
+package utez.edu.mx.citas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/fxAdmin")
+public class AdminController {
+     
+    @GetMapping("/")
+    public String index(){
+        return "citas/agenda";
+    }
+
+    @GetMapping("/index")
+    public String mostrarIndex() {
+        
+		return "redirect:/";
+	}
+}
