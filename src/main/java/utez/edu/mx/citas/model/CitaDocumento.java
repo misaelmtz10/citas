6656a3e6d6b74447115d0abf2424fac1359d4053
AlexCastroz <20183ti077@utez.edu.mx>
2022-04-15 14:43:16 -0500

    CRUD de servicios

---
+++ src/main/java/utez/edu/mx/citas/model/CitaDocumento.java
@@ -0,0 +1,87 @@
+package utez.edu.mx.citas.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "citas_has_documentos")
+public class CitaDocumento {
+	
+	@Id
+    @GeneratedValue
+    private long id;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "idCitas", nullable = false)
+    private Cita cita;
+	
+	/*@ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "idServicioDocumento", nullable = false)
+    private ServicioDocumento servicio_documento;*/
+	
+	@Column(columnDefinition = "tinyint not null")
+	private Integer estatus;
+	
+	@Column(nullable = true, length = 100)
+	private String archivo;
+
+	public CitaDocumento() {
+		super();
+	}
+
+	public CitaDocumento(long id, Cita cita, ServicioDocumento servicio_documento, Integer estatus, String archivo) {
+		super();
+		this.id = id;
+		this.cita = cita;
+		/*this.servicio_documento = servicio_documento;*/
+		this.estatus = estatus;
+		this.archivo = archivo;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Cita getCita() {
+		return cita;
+	}
+
+	public void setCita(Cita cita) {
+		this.cita = cita;
+	}
+
+	/*public ServicioDocumento getServicio_documento() {
+		return servicio_documento;
+	}
+
+	public void setServicio_documento(ServicioDocumento servicio_documento) {
+		this.servicio_documento = servicio_documento;
+	}*/
+
+	public Integer getEstatus() {
+		return estatus;
+	}
+
+	public void setEstatus(Integer estatus) {
+		this.estatus = estatus;
+	}
+
+	public String getArchivo() {
+		return archivo;
+	}
+
+	public void setArchivo(String archivo) {
+		this.archivo = archivo;
+	}
+
+}
---
+++ src/main/java/utez/edu/mx/citas/model/ServicioDocumento.java
@@ -0,0 +1,63 @@
+package utez.edu.mx.citas.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "servicios_has_documentos")
+public class ServicioDocumento {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+    private Long id;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "idServicos", nullable = false)
+    private Servicio servicio;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "idDocumentos", nullable = false)
+    private Documento documento;
+
+	public ServicioDocumento() {
+	}
+
+	public ServicioDocumento(Long id, Servicio servicio, Documento documento) {
+		this.id = id;
+		this.servicio = servicio;
+		this.documento = documento;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Servicio getServicio() {
+		return servicio;
+	}
+
+	public void setServicio(Servicio servicio) {
+		this.servicio = servicio;
+	}
+
+	public Documento getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(Documento documento) {
+		this.documento = documento;
+	}
+
+}
